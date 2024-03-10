10086462d976a94622b2cec77626ba27bc137edf
LazzyBones812 <99769744+LazzyBones812@users.noreply.github.com>
2024-03-10 22:47:17 +0300

    Update test_DOCUMENT_processing.py

--- project_1/test_DOCUMENT_processing.py
+++ project_1/test_DOCUMENT_processing.py
@@ -1,20 +1,20 @@
-import DOCUMENT_processing
+import main_2
 import os
 
 def test_1():
-    assert DOCUMENT_processing.main_work('3.pdf') == 'PDF файл успешно создан'
+    assert main_2.main_work('3.pdf') == 'Файл PDF обработан'
 
 def test_2():
-    assert DOCUMENT_processing.main_work(os.getcwd() + '\\' + '1.doc') == 'DOC файл успешно создан'
+    assert main_2.main_work(os.getcwd() + '\\' + '1.doc') == 'Файл DOC обработан'
 
 def test_3():
-    assert DOCUMENT_processing.main_work('1.docx') == 'DOCX файл успешно создан'
+    assert main_2.main_work('1.docx') == 'Файл DOCX обработан'
 
 def test_4():
-    assert DOCUMENT_processing.main_work('1.djvu') == 'DJVU файл успешно создан'
+    assert main_2.main_work('1.djvu') == 'Файл DJVU обработан'
 
 def test_5():
-    assert DOCUMENT_processing.main_work('404.djvu') == 'Такого файла не существует'
+    assert main_2.main_work('404.djvu') == 'Такого файла не существует'
 
 def test_6():
-    assert DOCUMENT_processing.main_work('1.pdq') == 'Такого файла не существует'
+    assert main_2.main_work('1.pdq') == 'Такого файла не существует'
